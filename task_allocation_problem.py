ede9de1a3024fbf90900294f2372670ee9a473e5
JunchengShen6 <junchengshen@hkaift.com>
2024-07-02 01:25:02 +0800

    add some function to use real city

--- task_allocation_problem.py
+++ task_allocation_problem.py
@@ -6,6 +6,7 @@ import pickle
 import random
 import os
 import numpy as np
+import pandas as pd
 from copy import deepcopy
 from my_wraps import timeit
 from task_alloacate_problem import SValue, TaskAllocationProblem
@@ -14,6 +15,10 @@ from task_alloacate_problem import SValue, TaskAllocationProblem
 
 
 class TaskRunner:
+    
+    city_num = 26
+    DIST_MATRIX_PATH = r"./data/中国各城市空间权重矩阵(1).xlsx"
+    PATH_CITY_DIST = f"./data/final/city_{city_num}.xlsx"
     # 本类的目标
     def __init__(self) -> None:
         pass
@@ -72,7 +77,7 @@ class TaskRunner:
                         )
                     # 保存 result 到 CSV 文件
                     self.save_to_csv(
-                        result,
+                        result, T_values, Z_values, S_n,
                         file_name=f"./data/result_per_iter/result_{J}_{Z}_{T}.csv",
                     )
                     s_value_memory = sys.getsizeof(s_value)
@@ -81,7 +86,7 @@ class TaskRunner:
                     print(f"{(T,Z)=} VFA_state_values memory usage: {VFA_state_values_memory} bytes")
         finally:
             # 将结果保存到 CSV 文件
-            self.save_to_csv(result, file_name="./data/final/results.csv")
+            self.save_to_csv(result, T_values, Z_values, S_n, file_name="./data/final/results.csv")
 
 
     def run(self, T, Z, J)->SValue:
@@ -110,6 +115,109 @@ class TaskRunner:
             with open(f"./data/save_params/s_value_s_values_{T}_{Z}_{J}.json", "w") as file:
                 json.dump(s_value.s_values, file, indent=4)
 
+    
+      
+    def get_proveng_city_dist_mat_df(self):
+        self.proveng_city_dist_mat_df = pd.read_excel(
+            self.DIST_MATRIX_PATH,
+            sheet_name="地理距离矩阵",
+        )
+
+    def get_city_series_to_city_dict(self):
+        self.city_series_to_city_dict = (
+            self.proveng_city_dist_mat_df[["cityeng", "cityseries"]]
+            .drop_duplicates()
+            .set_index("cityseries")["cityeng"]
+            .to_dict()
+        )
+    
+    def get_city_2_proveng_dict(self):
+        self.city_to_proveng_dict = (
+            self.proveng_city_dist_mat_df[["cityeng", "proveng"]]
+            .drop_duplicates()
+            .set_index("cityeng")["proveng"]
+            .to_dict()
+        )
+
+    def get_dist_mat_from_xls_df(self,):
+        self.distance_mat = self.proveng_city_dist_mat_df.iloc[:, self.distance_mat_start_idx:]
+        self.distance_mat.index = self.proveng_city_dist_mat_df.iloc[:, self.cityseries_idx].values
+        self.distance_mat.columns = self.proveng_city_dist_mat_df.iloc[:, self.cityseries_idx].values
+
+    def generate_city(self):
+        """本函数功能，生成指定数量城市的城市距离矩阵，必然包含上海。
+        返回的dataframe包含城市坐标和省份坐标/和一个城市转省份的dict"""
+
+        # 地理距离矩阵xls
+        self.get_proveng_city_dist_mat_df()
+        # 从第四列开始，是距离矩阵内容
+        self.proveng_idx = 0
+        self.cityeng = 1
+        self.cityseries_idx = 2
+        self.distance_mat_start_idx = 3
+
+        self.get_city_series_to_city_dict()
+        self.get_city_2_proveng_dict()
+
+        # 地理距离矩阵xls 第三列是城市id, city_*, 设定纵坐标
+        self.get_dist_mat_from_xls_df()
+        self.get_select_city_df()
+        
+        city_series_columns = self.select_city_df.columns
+        city_columns = [
+            (
+                self.city_to_proveng_dict[self.city_series_to_city_dict[item]],
+                self.city_series_to_city_dict[item],
+            )
+            for item in city_series_columns
+        ]  # 最为新矩阵的idx和col
+
+        column_names = ["proveng", "cityeng"]  # 给idx的idx
+        self.city_distance_df = pd.DataFrame(
+            self.select_city_df.values,
+            index=pd.MultiIndex.from_tuples(city_columns, names=column_names),
+            columns=pd.MultiIndex.from_tuples(city_columns, names=column_names),
+        )
+        
+        self.city_distance_df.to_excel(self.PATH_CITY_DIST)
+        print(self.PATH_CITY_DIST)
+
+    
+    def get_select_city_df(self):
+        # 挑选出安徽、江苏、浙江和上海的省份及对应的矩阵数据
+        select_proveng = ["Anhui", "Jiangsu", "Zhejiang", "Shanghai"]  # TODO 可能要换省份
+        provengs = self.proveng_city_dist_mat_df.iloc[
+            :, self.proveng_idx
+        ].values  # 表格第一列为城市名称
+        select_proveng_idxs = [
+            i for i, prov in enumerate(provengs) if prov in select_proveng
+        ]  # xls里选取指定的省份的idx，是字母
+        select_cityeng_idxs = self.proveng_city_dist_mat_df.values[
+            select_proveng_idxs, self.cityseries_idx
+        ]  # xls指定省份对应的城市id
+        select_idx = pd.Index(select_cityeng_idxs)  # 创建对应索引
+        # 使用 loc 方法选择相应的行和列
+        select_proveng_city_df = self.distance_mat.loc[select_idx, select_idx]
+
+        # 从城市列表中随机选择city_num-1个城市（不包括上海）
+        rnd_cities = np.random.choice(
+            select_cityeng_idxs[1:], self.city_num - 1, replace=False
+        )  # 第一列为上海，所以从1开始
+        # 将上海添加到随机选择的城市列表中 # City158 = shanghai
+        select_cities = ["City158"] + rnd_cities.tolist()
+        select_city_idx = pd.Index(select_cities)
+        # 使用 loc 方法选择相应的行和列
+        self.select_city_df = select_proveng_city_df.loc[select_city_idx, select_city_idx]
+
+    def read_arriving_rate_as_lambd():
+        
+        arriving_rate_df = pd.read_excel(
+            "./data/数据.xlsx", sheet_name="arriving rate", index_col=0
+        )
+        print(arriving_rate_df.shape)
+        
+        lambd = np.random.rand(26, 5)  # 生成率参数矩阵
+        print(lambd.shape)
     def init_a_problem(self, T=7, Z=3, J=10000):
         print("problem init")
         I_citys = 26
